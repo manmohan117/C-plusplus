02497938020fc9a5417b13492e68dc67f4aa1fda
manmohan117 <manmohan.singh@grazitti.com>
2026-01-30 07:22:48 +0000

    abstraction continues

--- Abstraction.cpp
+++ Abstraction.cpp
@@ -6,19 +6,33 @@ class Entity {
     public:
     //pure virtual
     virtual void display() = 0; 
+    // virtual int move(int x , int y) = 0;
 
 };
 
 class Player : public Entity{
+    private:
     void display() override {
         cout<<"Hi i am a player "<<endl;
     }
 };
 
+class NPC : public Entity {
+    private:
+    void display() override{
+
+        cout<<"HI i am a NPC ";
+    }
+};
+
 int main() {
-    
-    Entity* object1 = new Player();
-    object1->display();
+    Player player1 ;
+    NPC npc1;
+
+
+    Entity* entity = &npc1;
+    entity->display();
+
     return 0;
 }
 
